25a151870d0fe5fb1e7de8296b0c67817299d388
Your Name <you@example.com>
2020-05-08 12:42:17 +0530

    yupp

---
+++ yupp/calcy ts/calcy.ts
@@ -0,0 +1,16 @@
+function addition() {
+    document.getElementById("submit").value = ((+document.getElementById("number1").value)
+                                            + (+document.getElementById("number2").value));
+}
+function subtract() {
+    document.getElementById("submit").value = ((+document.getElementById("number1").value)
+                                            - (+document.getElementById("number2").value));
+}
+function multiply() {
+    document.getElementById("submit").value = ((+document.getElementById("number1").value)
+                                            * (+document.getElementById("number2").value));
+}
+function devide() {
+    document.getElementById("submit").value = ((+document.getElementById("number1").value)
+                                            / (+document.getElementById("number2").value));
+}
